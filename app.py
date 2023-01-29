e32b2acc94f7c67a0b810505d91f015674154c8f
Aswin <aswinks1995@outlook.com>
2023-01-28 22:07:19 -0500

    app modified

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def page2():
 
 def page3():
     st.subheader("About")
-    st.markdown("Created by Aswin KS. This project is deployed a a part of my college project,")
+    st.markdown("Created by Aswin KS")
     st.markdown("Tools: Azure, GIT, Jenkins, Docker, Linux, terraform")
 
 
